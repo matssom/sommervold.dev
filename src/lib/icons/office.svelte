b2ab2161e868e6cde8c1d7542f5b31f933b143b2
Mats Sommervold <mats@nordicbooster.com>
2025-03-04 22:14:47 +0100

    feat: initial commit

---
+++ src/lib/icons/office.svelte
@@ -0,0 +1,17 @@
+<symbol id="office" viewBox="0 0 24 24">
+    <path d="M24 24h-22v-24h14v8h8v16zm-16-5h-3v4h3v-4zm5 0h-3v4h3v-4zm8 0h-3v4h3v-4zm-15-5h-2v2h2v-2zm8 0h-2v2h2v-2zm-4 0h-2v2h2v-2zm9 0h-2v2h2v-2zm3 0h-2v2h2v-2zm-16-4h-2v2h2v-2zm8 0h-2v2h2v-2zm-4 0h-2v2h2v-2zm9 0h-2v2h2v-2zm3 0h-2v2h2v-2zm-16-4h-2v2h2v-2zm8 0h-2v2h2v-2zm-4 0h-2v2h2v-2zm-4-4h-2v2h2v-2zm8 0h-2v2h2v-2zm-4 0h-2v2h2v-2z"></path>
+</symbol>
+
+<svg class="icon">
+    <use xlink:href="#office" />
+</svg>
+
+<style>
+    .icon {
+        display: inline-block;
+        width: var(--icon-size, 2.5em);
+        height: var(--icon-size, 2.5em);
+        stroke-width: 0;
+        fill: currentColor;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	<div class="w-full h-full background-primary">
+		{@render children()}
+	</div>
+</ParaglideJS>
