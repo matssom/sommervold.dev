8eb7e987a631c9c4eeba64bbe3db38256373899b
Mats Sommervold <mats@sommervold.net>
2025-09-06 17:59:22 +0200

    fix: add vercel analytics and speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,7 @@
+import { injectAnalytics } from '@vercel/analytics/sveltekit'
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 export const prerender = true;
+
+injectAnalytics();
+injectSpeedInsights();
